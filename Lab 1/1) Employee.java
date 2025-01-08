a3b1f7b525834253c0c245c762b56859070bb008
Gulabi-Dil <thegreatmindhehe@gmail.com>
2025-01-08 18:31:45 +0530

    Update and rename Employee.java to 1) Employee.java

--- Lab 1/1) Employee.java
+++ Lab 1/1) Employee.java
@@ -1,3 +1,4 @@
+// Write a Java program to accept the number of hours worked, hourly rate and calculates the salary for an employee according to the following criteria: The company pays straight time for the first 40 hours worked by each employee and time and a half for all hours worked in excess of 40 hours.
 import java.util.Scanner;
 class Employee
 {
