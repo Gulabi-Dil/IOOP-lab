89b525a033ab752e145804033d23b6b8c93e2d1a
Gulabi-Dil <thegreatmindhehe@gmail.com>
2025-01-09 10:23:16 +0530

    Update 5) Stock.java

--- Lab 1/5) Stock.java
+++ Lab 1/5) Stock.java
@@ -14,13 +14,30 @@ class Stock
    double bill=0;
    System.out.println("Enter product names in separate lines:");
    for(int i=0;i<n;i++) name[i]=sc.nextLine();
-   System.out.println("\nEnter respective product prices in separate lines");
+   System.out.println("Enter respective product prices in separate lines");
    for(int i=0;i<n;i++) price[i]=sc.nextDouble();
-   System.out.print("\nEnter the number of specific distinct items you want to buy>   int choice=sc.nextInt();
+   System.out.print("Enter the number of specific distinct items you want to buy: ");
+   int choice=sc.nextInt();
    String cart[]=new String[choice];
    double total[]=new double[choice];
    sc.nextLine();
-   System.out.println("\nEnter the product names which you want to buy exactly as >   for(int i=0;i<choice;i++) cart[i]=sc.nextLine();
+   System.out.println("Enter the product names which you want to buy: ");
+   for(int i=0;i<choice;i++) cart[i]=sc.nextLine();
+   int c=0;
+   for(int i=0;i<choice;i++)
+   {
+     for(int j=0;j<n;j++)
+     {
+       if(name[j].equals(cart[i])) c++;
+     }
+   }
+   if(c<choice)
+   {
+     System.out.println("Invalid input. Some items you mentioned are not in stock.");
+     System.exit(0);
+   }
+   else
+   {
    for(int i=0;i<choice;i++)
    {
       for(int j=0;j<n;j++)
@@ -28,8 +45,8 @@ class Stock
          if(cart[i].equals(name[j])) total[i]=price[j];
          else continue;
       }
-   }
-   System.out.println("\nEnter respective product quantities:");
+   }}
+   System.out.println("Enter respective product quantities: ");
    for(int i=0;i<choice;i++) quant[i]=sc.nextInt();
    for(int i=0;i<choice;i++) bill+=quant[i]*total[i];
    System.out.println("Total bill is: "+ bill);
