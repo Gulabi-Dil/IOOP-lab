bc5f2f1028f655da525c530fea2a6e52409bacf8
Gulabi-Dil <thegreatmindhehe@gmail.com>
2025-01-12 15:55:16 +0530

    Update 5) Stock.java

--- Lab 1/5) Stock.java
+++ Lab 1/5) Stock.java
@@ -1,4 +1,7 @@
-// Write a Java program to manage stock information for multiple products. The program should store the product name, product price, and quantity in separate one-dimensional arrays for n items. The user should be able to specify which products they want to purchase and the desired quantity for each. Based on the user’s input, the program should generate and display the total bill.
+/* Write a Java program to manage stock information for multiple products. 
+The program should store the product name, product price, and quantity in separate one-dimensional arrays for n items. 
+The user should be able to specify which products they want to purchase and the desired quantity for each. 
+Based on the user’s input, the program should generate and display the total bill. */
 import java.util.Scanner;
 class Stock
 {
@@ -10,7 +13,6 @@ class Stock
    sc.nextLine();
    String name[]=new String[n];
    double price[]=new double[n];
-   int quant[]=new int[n];
    double bill=0;
    System.out.println("Enter product names in separate lines:");
    for(int i=0;i<n;i++) name[i]=sc.nextLine();
@@ -20,50 +22,44 @@ class Stock
    int choice=sc.nextInt();
    String cart[]=new String[choice];
    double total[]=new double[choice];
+   int quant[]=new int[choice];
    sc.nextLine();
    System.out.println("Enter the product names which you want to buy: ");
-   for(int i=0;i<choice;i++) cart[i]=sc.nextLine();
-   int c=0;
+   boolean check[]=new boolean[choice];
    for(int i=0;i<choice;i++)
    {
+     cart[i]=sc.nextLine();
+   }
+   for(int i=0;i<choice;i++)
+   {
+     boolean search=false;
      for(int j=0;j<n;j++)
      {
-       if(name[j].equals(cart[i])) c++;
-     }
-   }
-   if(c<choice)
-   {
-     System.out.println("Some items you requested for are not in stock. Computing bill of available items...");
-     for(int i=0;i<choice;i++)
-     {
-       for(int j=0;j<n;j++)
+       if(cart[i].equals(name[j]))
        {
-          if(cart[i].equals(name[j])) total[i]=price[j];
-          else
-          {
-            System.out.println(cart[i]+" is not in stock.");
-            break;
-          }
+         check[i]=true;
+         search=true;
+         break;
        }
      }
-     System.out.println("Enter respective product quantity/quantities: ");
-     for(int i=0;i<c;i++) quant[i]=sc.nextInt();
-     for(int i=0;i<c;i++) bill+=quant[i]*total[i];
-     System.out.println("Total bill is: "+bill);
+       if(search==false) System.out.println(cart[i]+" is not in stock.");
    }
-   else
-   {
    for(int i=0;i<choice;i++)
    {
-      for(int j=0;j<n;j++)
-      {
-         if(cart[i].equals(name[j])) total[i]=price[j];
-         else continue;
-      }
+       if(check[i])
+       {
+         System.out.println("Enter quantity of "+cart[i]+ ":");
+         quant[i]=sc.nextInt();
+         for(int j=0;j<n;j++)
+         {
+           if(cart[i].equals(name[j]))
+           {
+             bill+=quant[i]*price[j];
+             break;
+           }
+         }
+       }
    }
-   System.out.println("Enter respective product quantities: ");
-   for(int i=0;i<c;i++) quant[i]=sc.nextInt();
-   for(int i=0;i<c;i++) bill+=quant[i]*total[i];
-   System.out.println("Total bill is: "+ bill);
-  }}
+   System.out.println("Total bill is "+ bill);
+  }
 }
